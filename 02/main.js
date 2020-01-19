627a815e196c18cd9f220357289b9583f546efb4
Arthur <zkhomers@gmail.com>
2020-01-19 19:26:06 +0200

    object links

--- 02/main.js
+++ 02/main.js
@@ -1,29 +1,17 @@
-var text = "Enter a number";
-var arr3 = [+prompt(text), +prompt(text), +prompt(text)];
-
-
-var notebook = {
-    brand: prompt("brand"),
-    type:  prompt("type"),
-    model: prompt("Model"),
-    ram: prompt("ram"),
-    size: prompt("size"),
-    weight: Number(prompt("waight")),
-    resolution: {
-        width: Number(prompt("width")),
-        height: Number(prompt("height")),
-    },
-};
-
-var phone = {
-    brand: prompt("brand"),
-    model: prompt("model"),
-    ram: Number(prompt("ram")),
-    color: prompt("color"),
-};
-
-var person = {
-    name: prompt("name"),
-    surname: prompt("surname"),
-    married: confirm("married"),
+let person = {
+    name: prompt("Enter a name"),
+    surname: prompt("Enter a surname"),
+    married: confirm("Are you married?"),
+    smartphone: [
+        {
+            title: prompt('Enter a smartphone:'),
+        },
+    ],
+    laptop: [
+        {
+            title: prompt('Enter a laptop:'),
+        },
+    ],
 }
+person.smartphone.owner = person;
+person.laptop.owner = person;
---
+++ 07/main.js
@@ -0,0 +1,9 @@
+while(Math.random() > 0.9){
+    var i=0
+    i++
+    alert(i)
+    if (Math.random()>0.9) 
+    break
+
+   
+}
