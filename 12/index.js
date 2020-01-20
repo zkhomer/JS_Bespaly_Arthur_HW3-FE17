771253ab5bef312a21650db6f4812d66d66c0b00
Arthur <zkhomers@gmail.com>
2020-01-20 14:02:41 +0200

    chess

---
+++ 12/index.js
@@ -0,0 +1,24 @@
+function chess(w, h) {
+    let result = ''
+    for (let i = 1; i <= h; i++) {
+        if (i % 2) {
+            for (let a = 1; a <= w; a++) {
+                if (a % 2) {
+                    result += '.'
+                } else {
+                    result += '#'
+                }
+            }
+        } else {
+            for (let a = 1; a <= w; a++) {
+                if (a % 2) {
+                    result += '#'
+                } else {
+                    result += '.'
+                }
+            }
+        }
+        result += '\n'
+    }
+    return result;
+}
