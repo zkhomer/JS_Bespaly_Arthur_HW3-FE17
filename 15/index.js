9f693df40b60eb23cfcc6e21186f4d6114bd16e4
Arthur <zkhomers@gmail.com>
2020-01-21 15:45:53 +0200

    Задание на синий пояс: Треугольник

--- 15/index.js
+++ 15/index.js
@@ -1,10 +1,6 @@
-let max = 21
-    if (max  <= 0 || max % 2 == 0) {
-        alert("enter number")
-
-    }
-    let result = ""
-    for (let i = 1; i <= max; i += 2) {
-        let dots = (max - i) / 2
-        result += ".".repeat(dots) + "#".repeat(i) + ".".repeat(dots) + "\n"
-    }
+    let max = 21
+        let result = ""
+        for (let i = 1; i <= max; i += 2) {
+            let dots = (max - i) / 2
+            result += ".".repeat(dots) + "#".repeat(i) + ".".repeat(dots) + "\n"
+        }
